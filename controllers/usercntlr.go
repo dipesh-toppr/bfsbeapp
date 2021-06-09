aecbb470d1b77f0bc097820c9c140a9f679a82db
dipesh-toppr <dipesh.gupta@toppr.com>
2021-06-09 13:40:29 +0530

    remove conflicts

--- controllers/usercntlr.go
+++ controllers/usercntlr.go
@@ -10,25 +10,25 @@ import (
 )
 
 // Signup allows the user to create an account.
-func Signup(w http.ResponseWriter, r *http.Request) {
+func Signup(response http.ResponseWriter, request *http.Request) {
 
 	// var u models.User
 	// process form submission
-	if r.Method == http.MethodPost {
-		var u models.User
-		u, err := managers.SaveUser(r)
+	if request.Method == http.MethodPost {
+		var user models.User
+		user, err := managers.SaveUser(request)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(response, err.Error(), http.StatusBadRequest)
 			fmt.Println("SignUp Failed")
 			return
 		}
 
 		// add token to cookies
-		token.Createtoken(u, w)
-		w.WriteHeader(http.StatusOK)
+		token.Createtoken(user, response)
+		response.WriteHeader(http.StatusOK)
 
-		fmt.Println(u)
-		w.Write([]byte("SignUp Successful"))
+		fmt.Println(user)
+		response.Write([]byte("SignUp Successful"))
 		fmt.Println("SignUp Successful")
 
 		// redirect
@@ -38,42 +38,42 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 }
 
 // Login allows registered user to access the application.
-func Login(w http.ResponseWriter, r *http.Request) {
+func Login(response http.ResponseWriter, request *http.Request) {
 
 	// var u models.User
 	// process form submission
-	if r.Method == http.MethodPost {
+	if request.Method == http.MethodPost {
 
-		p := r.FormValue("password")
-		e := r.FormValue("email")
+		password := request.FormValue("password")
+		email := request.FormValue("email")
 
 		// check if the user exists
-		u, ok := managers.FindUser(e)
+		user, ok := managers.FindUser(email)
 		if !ok {
-			http.Error(w, "username and/or password do not match", http.StatusForbidden)
+			http.Error(response, "username and/or password do not match", http.StatusForbidden)
 			fmt.Println("Logined Failed")
 			return
 		}
 
-		d, _ := managers.IsDisabled(u)
-		if d {
+		disable, _ := managers.IsDisabled(user)
+		if disable {
 			// http.Error(w, err.Error(), http.StatusForbidden)
-			http.Error(w, "user is disabled by admin....", http.StatusForbidden)
+			http.Error(response, "user is disabled by admin....", http.StatusForbidden)
 			fmt.Println("user is disabled by admin....")
 			return
 		}
 
-		if !managers.ValidatePassword(u, p) {
-			http.Error(w, "username and/or password do not match", http.StatusForbidden)
+		if !managers.ValidatePassword(user, password) {
+			http.Error(response, "username and/or password do not match", http.StatusForbidden)
 			fmt.Println("Logined Failed")
 			return
 		}
 
 		// add token to cookies
-		token.Createtoken(u, w)
-		w.WriteHeader(http.StatusOK)
+		token.Createtoken(user, response)
+		response.WriteHeader(http.StatusOK)
 
-		w.Write([]byte("Login Successful"))
+		response.Write([]byte("Login Successful"))
 		fmt.Println("Login Successful")
 
 		// http.Redirect(w, r, "/", http.StatusOK)
@@ -83,16 +83,16 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 // Logout method to call when the user signed out of the application.
-func Logout(w http.ResponseWriter, r *http.Request) {
+func Logout(response http.ResponseWriter, request *http.Request) {
 
-	http.SetCookie(w, &http.Cookie{
+	http.SetCookie(response, &http.Cookie{
 		Name:     "token",
 		Value:    "",
 		MaxAge:   -1,
 		HttpOnly: true,
 	})
-	w.Write([]byte("LogOut Successful"))
+	response.Write([]byte("LogOut Successful"))
 	fmt.Println("LogOut Successful")
-	w.WriteHeader(http.StatusOK)
+	response.WriteHeader(http.StatusOK)
 
 }
